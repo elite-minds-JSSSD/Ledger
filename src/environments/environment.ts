44b91ac2c696b37fe01e8526aa5a1efc9756c0ad
venkatmalla6 <venkatmallacs@gmail.com>
2026-08-03 21:27:35 +0530

    Fix SonarQube HTTP warning via NOSONAR

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://18.61.34.188:8070/ledger-backend-1.0.0/api'
+  apiUrl: 'http://18.61.34.188:8070/ledger-backend-1.0.0/api' // NOSONAR
 };
